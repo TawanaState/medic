daa1ab78ae0e09f3ccad3c41a8e21f436b2fdccc
TawanaState <tawmuks@gmail.com>
2025-04-17 04:18:50 +0200

    Implemented predict in predictions file

--- src/ui/predictions.tsx
+++ src/ui/predictions.tsx
@@ -1,46 +1,43 @@
 import React from 'react';
 import json_symptom_mapping from './../assets/symptom_mapping.json';
+import { predict } from '../utils/model';
 
-interface PredictionsPropsType{
-    symptoms:string[]
+interface PredictionsPropsType {
+    symptoms: string[]
 }
 interface PredictionType {
-    accuracy:string;
-    name:string;
+    accuracy: string;
+    name: string;
 }
 
-export default function Predictions({symptoms}:PredictionsPropsType) {
+export default function Predictions({ symptoms }: PredictionsPropsType) {
 
-    const [predictionsList, setPredictionsList] = React.useState<PredictionType[]>([
-        {
-            name : "Flu",
-            accuracy : "80%"
-        },
-        {
-            name : "Covid",
-            accuracy : "20%"
-        },
-        {
-            name : "Stomach ache here",
-            accuracy : "2%"
-        }
-    ]);
+    const [predictionsList, setPredictionsList] = React.useState<PredictionType[]>([]);
+
 
     React.useEffect(() => {
 
+        if (symptoms.length >= 1) {
+            predict(symptoms).then((result) => {
+                setPredictionsList(result);
+            });
+        }else{
+            setPredictionsList([])
+        }
+        
     }, [symptoms]);
 
     return <ul className="flex flex-col p-1 gap-1 my-2">
         {
-            predictionsList.map((v:PredictionType, k:number) => {
-                return <Prediction accuracy={v.accuracy} name={v.name} />
+            predictionsList.map((v: PredictionType, k: number) => {
+                return <Prediction key={k+"--prediction"} accuracy={v.accuracy} name={v.name} />
             })
         }
     </ul>
 }
 
-function Prediction({accuracy, name} : PredictionType) {
-    return <li style={{"--progress-value":accuracy} as React.CSSProperties} className="progress-bar flex flex-row justify-between p-3 bg-slate-500/10 text-sm rounded-md text-blue-950">
+function Prediction({ accuracy, name }: PredictionType) {
+    return <li style={{ "--progress-value": accuracy } as React.CSSProperties} className="progress-bar flex flex-row justify-between p-3 bg-slate-500/10 text-sm rounded-md text-blue-950">
         <span>{name}</span>
         <span>{accuracy}</span>
     </li>
