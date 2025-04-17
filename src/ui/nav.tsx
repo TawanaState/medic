b7841737b8afa5046272a605068546f56c3c0713
TawanaState <tawmuks@gmail.com>
2025-04-17 10:56:05 +0200

    Changed Code Link

--- src/ui/nav.tsx
+++ src/ui/nav.tsx
@@ -1,6 +1,6 @@
 export default function Nav() {
     return <nav className="flex flex-row justify-between">
         <a href="https://tawananyasha.com/" className="btn">Developer</a>
-        <a href="hhttps://github.com/TawanaState/medic/" className="btn">Code</a>
+        <a href="https://github.com/TawanaState/medic/" className="btn">Code</a>
     </nav>
 }
